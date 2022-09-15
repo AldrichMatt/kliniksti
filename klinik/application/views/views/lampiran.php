ad2c91396681c2609bd6c5da69f5d5d12c10a582
AldrichMatt <95916778+AldrichMatt@users.noreply.github.com>
2022-09-15 15:46:23 +0800

    halaman baru
    Added kasir.php, pages/kasir.php, little update on footer.php and header.php

---
+++ klinik/application/views/views/lampiran.php
@@ -0,0 +1,18 @@
+<?php include "bootstrap/header.php"; ?>
+<?php include "bootstrap/navbar.php"; ?>
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper container-xxl p-0">
+    <?php include "bootstrap/breadcrumbs.php"; ?>
+    <?php include "pages/lampiran.php"; ?>
+        
+    </div>
+</div>
+<script>
+    $(window).on('load', function(){
+        let title = 'lampiran';
+        $('#'+title).addClass('active');
+    })
+</script>
+<?php include "bootstrap/footer.php"; ?>
---
+++ klinik/application/views/views/pages/riwayat_kunjungan.php
@@ -0,0 +1,27 @@
+ <div class="content-body">
+            <section class="dashboard-ecommerce">
+                <div class="card p-2">
+                    <?php include "bootstrap/profile.php"; ?>
+                    <?php include "bootstrap/tab.php"; ?>
+                </div>
+                <table class="table table-stripped table-hover my-3">
+                        <thead>
+                            <th>Unit</th>
+                            <th>Tanggal</th>
+                            <th>Dokter</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>POLI UMUM</td>
+                                <td>DD-MM-YYYY HH:MM:SS</td>
+                                <td>Dr. John Doe</td>
+                            </tr>
+                            <tr>
+                                <td>POLI UMUM</td>
+                                <td>DD-MM-YYYY HH:MM:SS</td>
+                                <td>Dr. John Doe</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </section>
+        </div>
